6348ab421d05233022ba169fbfed6f4831f55ffc
alkilany <herosmash98@gmail.com>
2023-10-05 18:52:33 +0300

    first day in c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/**
+ * main - "\'prints and that piece of art is useful" - Dora Korpar, 2015-10-19" 
+ * Returun: (1) if succesful
+ */
+
+#include<stdio.h>
+
+int main()
+{
+	if(1 == 1)
+	{	
+	perror("and that piece of art is useful\" - Dora Korpar,
+	2015-10-19\n");
+	}
+	return (1);
+}
